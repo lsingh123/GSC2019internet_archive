d7fffa4397e28d6956cf9b6392be32dfb00373b7
Lavanya Singh <lavanyasingh@Lavanyas-MacBook-Air-011219.local>
2019-07-24 16:42:49 -0700

    linted

--- meta/get_fbog_pool2.py
+++ meta/get_fbog_pool2.py
@@ -12,25 +12,26 @@ import csv
 from requests_html import HTMLSession
 import time
 
+
 class FBOGCrawler():
-    
+
     PATH = "/Users/lavanyasingh/Desktop/GSC2O19internet_archive/data/raw/"
-    
+
     def __init__(self, processes):
         self.res, self.urls = [], []
         self.session = HTMLSession()
         self.read_in()
         self.processes = processes
-        
+
     def read_in(self):
         with open(self.PATH + "all_raw_cleaned3.csv", 'r') as f:
             reader = csv.reader(f, delimiter=',')
             for line in reader:
-                #for testing purposes
-                #if len(self.urls) > 100: break
+                # for testing purposes
+                # if len(self.urls) > 100: break
                 self.urls.append("http://" + "".join(line[1]))
         print("DONE READING")
-    
+
     def get_attr(self, head, attr):
         try:
             return head.find(attrs={"property": "og:" + attr})['content']
@@ -44,17 +45,17 @@ class FBOGCrawler():
             return head.find("title").text
         except AttributeError:
             return ""
-    
+
     def get_locale(self, head):
-        try: 
+        try:
             return head.find(attrs={"property": "og:locale"})['content']
         except TypeError:
             return ""
-    
+
     def get_meta(self, url):
         try:
-            response = self.session.get(url, timeout = 30)
-            soup = BeautifulSoup(response.html.html, features = "html.parser")
+            response = self.session.get(url, timeout=30)
+            soup = BeautifulSoup(response.html.html, features="html.parser")
             head = soup.head
             title = self.get_attr(head, "title")
             desc = self.get_attr(head, "description")
@@ -62,14 +63,15 @@ class FBOGCrawler():
             return [url, title, desc, locale]
         except Exception as e:
             return [url, str(e)]
-    
+
     def write_meta(self):
         with open(self.PATH + "meta_good.csv", 'w') as outf:
-            w = csv.writer(outf, delimiter= ',', quotechar = '"', quoting = csv.QUOTE_MINIMAL)
+            w = csv.writer(outf, delimiter=',', quotechar='"',
+                           quoting=csv.QUOTE_MINIMAL)
             for url in self.res:
                 w.writerow(url)
         print("WROTE ALL META")
-    
+
     def main(self):
         p = Pool(processes=self.processes)
         time1 = time.time()
@@ -81,7 +83,8 @@ class FBOGCrawler():
         self.write_meta()
         self.session.close()
 
+
 if __name__ == "__main__":
-    crawler = FBOGCrawler(processes = 2)
+    crawler = FBOGCrawler(processes=2)
     crawler.main()
     print("FINISHED RUNNING")
